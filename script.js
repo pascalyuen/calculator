3c327dc4744cd84203e26d319b95feb3e488058a
Pascal <pascalyuen@gmail.com>
2022-08-19 03:24:05 +0800

    Fix display area

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Numbers
+// const zero = document.querySelector(.0);
+// const one = document.querySelector(.1);
+// const two = document.querySelector(.2);
+// const three = document.querySelector(.3);
+// const four = document.querySelector(.4);
+// const five = document.querySelector(.5);
+// const six = document.querySelector(.6);
+// const seven = document.querySelector(.7);
+// const eight = document.querySelector(.8);
+// const nine = document.querySelector(.9);
+const numbers = document.querySelectorAll('.number');
+const displayText = document.querySelector('.display-text');
+
+
+// Actions
+numbers.forEach(number => number.addEventListener('click', showNumber));
+
+
+// Functions
 function add(a, b) {
     return a + b;
 }
@@ -19,4 +39,8 @@ function operate(operator, a, b) {
     if (operator === '-') return substract(a, b);
     if (operator === '*') return multiply(a, b);
     if (operator === '/') return divide(a, b);
+}
+
+function showNumber(e) {
+    displayText.textContent = e.target.textContent;
 }
